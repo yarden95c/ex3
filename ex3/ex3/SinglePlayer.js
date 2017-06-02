04f514426bffbe7f51101e4ae0c8eb811bdbed71
yarden cohen <yarden95c@gmail.com>
2017-06-02 12:34:55 +0300

    myPage + menu

---
+++ ex3/ex3/SinglePlayer.js
@@ -0,0 +1,17 @@
+﻿$("nav").load("Menu.html");
+function drawMaze(maze) {
+    var myCanvas = document.getElementById("mazeCanvas");
+    var context = mazeCanvas.getContext("2d");
+    var rows = maze.length;
+    var cols = maze[0].length;
+    var cellWidth = mazeCanvas.width / cols;
+    var cellHeight = mazeCanvas.height / rows;
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            if (maze[i][j] == 1) {
+                context.fillRect(cellWidth * j, cellHeight * i,
+                    cellWidth, cellHeight);
+            }
+        }
+    }
+}
